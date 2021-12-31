b0b897db25e0ebe5d19368b71eb09c8f62b5dcca
eiloncohen <eiloncohen20@gmail.com>
2021-12-31 12:38:57 +0200

    add  difficulty level

--- main.py
+++ main.py
@@ -5,15 +5,20 @@ from scoreboard import ScoreBored
 import time
 
 
-# initialize
+# initialize the screen game
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+
+level = screen.textinput("Snake Game", "Please enter a difficulty level: (easy/medium/hard/expert)")
+
 scoreboard = ScoreBored()
 total_score = 0
+
 screen.tracer(0)
 
+# create the snake obj and connect the events of keys to him
 new_snake = Snake()
 screen.listen()
 screen.onkey(new_snake.up, "Up")
@@ -21,21 +26,26 @@ screen.onkey(new_snake.down, "Down")
 screen.onkey(new_snake.left, "Left")
 screen.onkey(new_snake.right, "Right")
 screen.screensize(300,300)
+
+# create food obj and ready to start the game.
 food = Food()
 game_is_on = True
-
+level_time_waiting = scoreboard.choose_level_game(level)
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(level_time_waiting)
     screen.update()
+    # move method is responsible on snake movement behavior
     new_snake.move()
+
+    # check if collision happened (with the tail of the snake or with the wall)
     xcor_head_snake = new_snake.snake_body[0].xcor()
     ycor_head_snake = new_snake.snake_body[0].ycor()
     if xcor_head_snake < -280 or xcor_head_snake > 280 or ycor_head_snake < -280 or ycor_head_snake > 280 or new_snake.if_own_collision_happend():
         scoreboard.game_over()
         game_is_on = False
 
-
+    # check if snake ate the food
     food.check_if_eaten(new_snake, scoreboard)
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -21,3 +21,15 @@ class ScoreBored(Turtle):
     def game_over(self):
         self.setposition(0, 0)
         self.write("Game Over.", move=False, align='center', font=("Verdana", 30, "normal"))
+
+    def choose_level_game(self, level):
+        if level == "easy":
+            return 0.2
+        elif level == "medium":
+            return 0.1
+        elif level == "hard":
+            return 0.07
+        elif level == "expert":
+            return 0.04
+        else:
+            raise Exception("wrong level! enter level again")
